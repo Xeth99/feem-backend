7f6bb206b7dacb22e0bb3f61875ad3f335850d09
xeth99 <emmanuelfemi646@gmail.com>
2025-03-06 15:01:47 +0100

    updates made on the server

--- api/index.js
+++ api/index.js
@@ -1,36 +1,48 @@
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import serverless from "serverless-http";
+
+// import { connectDB } from "../config/db.js";
+// import userRouter from "../routers/userRouter.js";
+// import movieRouter from "../routers/movieRouter.js";
+// import categoriesRouter from "../routers/categoriesRouter.js";
+// import Uploadrouter from "../controllers/uploadFile.js";
+// import { errorHandler } from "../middlewares/errorMiddleware.js";
+
+// dotenv.config();
+
+// connectDB();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Api is running...");
+// });
+
+// app.get("/favicon.ico", (req, res) => res.status(204).end());
+
+// app.use("/users", userRouter);
+// app.use("/movies", movieRouter);
+// app.use("/categories", categoriesRouter);
+// app.use("/upload", Uploadrouter);
+
+// // Error handling middleware
+// app.use(errorHandler);
+
+// export default serverless(app);
+
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
 import serverless from "serverless-http";
 
-import { connectDB } from "../config/db.js";
-import userRouter from "../routers/userRouter.js";
-import movieRouter from "../routers/movieRouter.js";
-import categoriesRouter from "../routers/categoriesRouter.js";
-import Uploadrouter from "../controllers/uploadFile.js";
-import { errorHandler } from "../middlewares/errorMiddleware.js";
-
-dotenv.config();
-
-// await connectDB();
-
 const app = express();
-
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Api is running...");
 });
 
-app.get("/favicon.ico", (req, res) => res.status(204).end());
-
-app.use("/users", userRouter);
-app.use("/movies", movieRouter);
-app.use("/categories", categoriesRouter);
-app.use("/upload", Uploadrouter);
-
-// Error handling middleware
-app.use(errorHandler);
-
 export default serverless(app);
+
