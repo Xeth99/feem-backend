0f3396d3f1abe497728fc6860be55c39a48efc57
xeth99 <emmanuelfemi646@gmail.com>
2025-04-10 10:57:40 +0100

    Add TMDB movie routes

--- routers/movieRouter.js
+++ routers/movieRouter.js
@@ -6,46 +6,65 @@ import fetchFromTMDB from "../config/tmdbServices.js";
 const router = express.Router();
 
 // ************ PUBLIC ROUTES FOR TMBD ************
+// get all movies
+router.get("/tmdb", async (req, res) => {
+  const { language, page, region, with_genres } = req.query;
+  try {
+    const data = await fetchFromTMDB("/movie/now_playing", {
+      language,
+      region,
+      page,
+      with_genres,
+    });
+    res.json(data.results);
+  } catch (error) {
+    console.error("TMDB Error:", error.response?.data || error.message);
+    res.status(500).json({
+      error: "Failed to fetch movies",
+      details: error.message,
+    });
+  }
+});
 // popular movies
 router.get("/tmdb/popular", async (req, res) => {
-    try {
-      const data = await fetchFromTMDB("/movie/popular");
-      res.json(data.results);
-    } catch (error) {
-      console.error("TMDB Error:", error.response?.data || error.message);
-      res.status(500).json({ 
-        error: "Failed to fetch movies",
-        details: error.message 
-      });
-    }
+  try {
+    const data = await fetchFromTMDB("/movie/popular");
+    res.json(data.results);
+  } catch (error) {
+    console.error("TMDB Error:", error.response?.data || error.message);
+    res.status(500).json({
+      error: "Failed to fetch movies",
+      details: error.message,
+    });
+  }
 });
-  
+
 // get movie by id
 router.get("/tmdb/:id", async (req, res) => {
-    try {
-      const data = await fetchFromTMDB(`/movie/${req.params.id}`);
-      res.json(data);
-    } catch (error) {
-      console.error("TMDB Error:", error.response?.data || error.message);
-      res.status(500).json({ 
-        error: "Failed to fetch movie",
-        details: error.message 
-      });
-    }
+  try {
+    const data = await fetchFromTMDB(`/movie/${req.params.id}`);
+    res.json(data);
+  } catch (error) {
+    console.error("TMDB Error:", error.response?.data || error.message);
+    res.status(500).json({
+      error: "Failed to fetch movie",
+      details: error.message,
+    });
+  }
 });
 
 // get top rated movies
 router.get("/tmdb/rated/top", async (req, res) => {
-    try {
-      const data = await fetchFromTMDB("/movie/top_rated");
-      res.json(data.results);
-    } catch (error) {
-      console.error("TMDB Error:", error.response?.data || error.message);
-      res.status(500).json({ 
-        error: "Failed to fetch movies",
-        details: error.message 
-      });
-    }
+  try {
+    const data = await fetchFromTMDB("/movie/top_rated");
+    res.json(data.results);
+  } catch (error) {
+    console.error("TMDB Error:", error.response?.data || error.message);
+    res.status(500).json({
+      error: "Failed to fetch movies",
+      details: error.message,
+    });
+  }
 });
 
 // ************ PUBLIC ROUTES ************
